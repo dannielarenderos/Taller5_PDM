b1008f59df759ddf328ecdadf3c7d262d3f00bed
dannielarenderos <00087717@uca.edu.sv>
2019-06-12 01:53:35 -0600

    Implementacion de Navigation Editor

---
+++ app/src/main/java/com/avgh/naveditortry/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.avgh.naveditortry
+
+import android.app.PendingIntent.getActivity
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+}
